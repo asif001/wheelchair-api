c1d16e8e58dbe3a46eac5ae1dad427aa1e318a07
Asifur Rahman <mail11asifur@gmail.com>
2019-06-29 22:07:57 +0600

    vales as a class memeber

--- resourses/todo.py
+++ resourses/todo.py
@@ -2,50 +2,51 @@ from flask import request, jsonify
 from flask_restful import Resource
 
 
-values = {'acl_val': 0.0, 'fall_status': 0, 'isTriggered': 0}
-
-
 class Status(Resource):
+    values = {'acl_val': 0.0, 'fall_status': 0}
+    
     def get(self):
-        if values['fall_status'] == 0:
-            if 110 <= values['acl_val'] <= 250:
-                values['fall_status'] = 1
+        if self.values['fall_status'] == 0:
+            if 110 <= self.values['acl_val'] <= 250:
+                self.values['fall_status'] = 1
                 data = {
-                    "Accelerometer": values['acl_val'],
-                    "FallStatus": values['fall_status'],
+                    "Accelerometer": self.values['acl_val'],
+                    "FallStatus": self.values['fall_status'],
                 }
                 resp = jsonify(data)
                 resp.status_code = 200
                 return resp
             else:
-                values['fall_status'] = 0
+                self.values['fall_status'] = 0
                 data = {
-                    "Accelerometer": values['acl_val'],
-                    "FallStatus": values['fall_status'],
+                    "Accelerometer": self.values['acl_val'],
+                    "FallStatus": self.values['fall_status'],
                 }
                 resp = jsonify(data)
                 resp.status_code = 200
                 return resp
         else:
             data = {
-                "Accelerometer": values['acl_val'],
-                "FallStatus": values['fall_status'],
+                "Accelerometer": self.values['acl_val'],
+                "FallStatus": self.values['fall_status'],
             }
             resp = jsonify(data)
             resp.status_code = 200
             return resp
 
     def post(self):
-        values['acl_val'] = float(request.form["Accelerometer"])
+        self.values['acl_val'] = float(request.form["Accelerometer"])
 
 
 class Trigger(Resource):
+    values = {'isTriggered': 0}
+
     def post(self):
-        values['isTriggered'] = int(request.form["isTriggered"])
+        self.values['isTriggered'] = int(request.form["isTriggered"])
 
     def get(self):
         data = {
-            "isTriggered": values['isTriggered'],
+            "isTriggered": self.values['isTriggered'],
         }
         resp = jsonify(data)
         resp.status_code = 200
